6fe94287e380dc493de25546efa2bf8ff5aa82a8
Fernando Marins <fernandomarins@Fernandos-MacBook-Pro.local>
2021-12-07 15:46:47 -0300

    modified gitignore to stop tracking Constants.swift, added ConstantsTemplate.swift instead

--- youtube-app/ConstantsTemplate.swift
+++ youtube-app/ConstantsTemplate.swift
@@ -1,16 +1,21 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-app
 //
 //  Created by Fernando Marins on 07/12/21.
 //
 
+/*
+ Add your own keys below and uncomment the code
+ */
 import Foundation
 
+/*
 struct Constants {
     
-    static var apiKey = "AIzaSyCWvl7aPpsR-gtys61EfwMHzkAN8Z6dOEk"
-    static var playlistId = "UU2D6eRvCeMtcF5OGHf1-trw"
+    static var apiKey = ""
+    static var playlistId = ""
     static var url = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.playlistId)&key=\(Constants.apiKey)"
     
 }
+ */
